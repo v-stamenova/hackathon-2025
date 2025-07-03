9d023826982c7711f9d8829bc4b34fde25a51ef5
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-07-03 22:02:37 +0200

    Install app laravel

--- frontend/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-
-	let { children } = $props();
-</script>
-
-<div class='h-screen bg-linear-to-b from-yellow-100 to-yellow-500 '>
-	{@render children()}
-</div>
